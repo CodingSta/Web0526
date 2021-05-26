820fe397b86487838d0da85c2aad905cecb2719e
MinGyu, Ju <knighp@gmail.com>
2021-05-26 14:40:39 +0900

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace Web0526
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello BTS!");
+                    await context.Response.WriteAsync("Hello Black Pink!");
                 });
             });
         }
